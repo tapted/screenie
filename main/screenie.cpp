fb3aea936559de367189363f11bc32b5419b3eb0
Trent Apted <tapted@gmail.com>
2026-08-23 18:26:42 +1000

    Enable OTA

--- main/screenie.cpp
+++ main/screenie.cpp
@@ -15,20 +15,16 @@ extern "C" void app_main(void) {
   system_diagnostic_free_iram_at_boot = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
   initialize_network_logger(4096, false);
   // delayed_pm_enable();
-  start_ota_rollback_watchdog(2, 120000);
+  start_ota_rollback_watchdog(2);
 
-  // ota_controller = new HAPPY::Entities::OtaController(screenie_device, "1.0.0");
   install_screenie_sensors();
 
-  network.time_sync_callback = [](struct timeval* /*tv*/) {
+  network.time_sync_callback = [](struct timeval*) {
     startup_gate_passed("NTP Time Synced");
-
     publish_screenie_sensors(true);
-
-    network.time_sync_callback = [](struct timeval* /*tv*/) { publish_screenie_sensors(true); };
+    network.time_sync_callback = nullptr;
   };
 
-  // Entities must be registered before the network is started so discovery messages are not missed.
   network.start();
 
   ESP_LOGI(TAG, "Starting main loop...");
--- main/screenie_sensors.cpp
+++ main/screenie_sensors.cpp
@@ -3,20 +3,22 @@
 #include "espbase/main_loop_task.hpp"
 #include "halpp/config.hpp"
 #include "happy/entities/system_diagnostics.hpp"
+#include "happy/entities/ota.hpp"
 #include "screenie_device.hpp"
 #include "screenie_hardware.hpp"
-
+  
 using halpp::config;
 
 static HAPPY::Entities::SystemDiagnostics* diagnostics = nullptr;
+static HAPPY::Entities::OtaController* ota_controller = nullptr;
 
 static constinit MainLoopTask<void> publish_sensors_on_time_interval;
 
 void install_screenie_sensors() {
   init_screenie_hardware();
 
+  ota_controller = new HAPPY::Entities::OtaController(screenie_device, "1.0.0");
   diagnostics = new HAPPY::Entities::SystemDiagnostics(screenie_device);
-
   publish_sensors_on_time_interval.start({.name = "publish_sensors"}, nullptr,
                                          [](auto&) -> std::optional<uint32_t> {
                                            publish_screenie_sensors();
